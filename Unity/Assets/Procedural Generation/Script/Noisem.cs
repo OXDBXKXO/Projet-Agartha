9b5d27a99f4f986f6ccc07c375181a7eec0b109f
Grysby <32579921+Grysby@users.noreply.github.com>
2018-03-12 15:10:10 +0100

    Procedural generation
    Add some parameters in my previous function in order to make it better

--- Unity/Assets/Procedural Generation/Script/Noisem.cs
+++ Unity/Assets/Procedural Generation/Script/Noisem.cs
@@ -4,22 +4,44 @@ using UnityEngine;
 
 public static class Noisem  {
 
-	public static float[,] genertnoisemap(int width, int height, float divider) {
-        float[,] nmap = new float[width , height];
+    public static float[,] genertnoisemap(int width, int height, float divider, int shaders, float persi, float lacu) {
+        float[,] nmap = new float[width, height];
         if (divider <= 0)
             divider = (float)0.0000001;
-        for(int y = 0; y < height; y++)
+        float mnh = float.MinValue;
+        float minnh = float.MaxValue;
+        for (int y = 0; y < height; y++)
         {
-            for(int x = 0; x < width; x++)
+            for (int x = 0; x < width; x++)
             {
-                float noix = x/divider;
-                float noiy = y/divider;
-                float val = Mathf.PerlinNoise(noix, noiy);
-                nmap[x, y] = val;
+                float ampli = 1;
+                float frequen = 1;
+                float nheigth = 0;
 
+                for (int i = 0; i < shaders; i++)
+                {
+                    float noix = x / divider * frequen;
+                    float noiy = y / divider * frequen;
+                    float val = Mathf.PerlinNoise(noix, noiy) * 2 - 1;
+                    nheigth = val * ampli;
+                    ampli = ampli * frequen;
+                    frequen = frequen * lacu;
+                }
+                if (nheigth > mnh)
+                { mnh = nheigth; }
+                else if (nheigth < minnh)
+                { minnh = nheigth; }
+                nmap[x, y] = nheigth;
             }
         }
-        return nmap;
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                nmap[x, y] = Mathf.InverseLerp(minnh, mnh, nmap[x, y]);
+            }
+        }
+                return nmap;
     }
     
       
--- Unity/Assets/Procedural Generation/Script/mapgenerator.cs
+++ Unity/Assets/Procedural Generation/Script/mapgenerator.cs
@@ -6,10 +6,14 @@ public class mapgenerator : MonoBehaviour {
     public int width;
     public int height;
     public float scale;
+    public int shaders;
+    public float persi;
+    public float lacu;
+    public bool isupdated;
 
 	public void gennmap()
     {
-        float[,] nmap = Noisem.genertnoisemap(width, height, scale);
+        float[,] nmap = Noisem.genertnoisemap(width, height, scale,shaders,persi,lacu);
         thedisplay disp = FindObjectOfType<thedisplay>();
         disp.buildnmap(nmap);
     }
