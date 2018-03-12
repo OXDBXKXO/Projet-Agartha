4f49dfd24c953ca6d65a4871ab9163436bced7ee
Grysby <32579921+Grysby@users.noreply.github.com>
2018-03-12 18:05:09 +0100

    The begining of procedural generation
    By using noise map method, colors have been added to the map.

--- Unity/Assets/Procedural Generation/Script/thedisplay.cs
+++ Unity/Assets/Procedural Generation/Script/thedisplay.cs
@@ -5,23 +5,11 @@ using UnityEngine;
 public class thedisplay : MonoBehaviour {
 
     public Renderer txture;
-    public void buildnmap(float[,] nmap)
+    public void buildmap(Texture2D txt)
     {
-        int width = nmap.GetLength(0);
-        int height = nmap.GetLength(1);
-        Texture2D texture = new Texture2D(width, height);
-        Color[] colorm = new Color[width * height];
-        for(int y = 0;y < height;y++)
-        {
-            for(int x = 0;x < width;x++)
-            {
-                colorm[y * width + x] = Color.Lerp(Color.black, Color.white, nmap[x, y]);
-            }
-        }
-        texture.SetPixels(colorm);
-        texture.Apply();
-        txture.sharedMaterial.mainTexture = texture;
-        txture.transform.localScale = new Vector3(width, 1, height);
+       
+        txture.sharedMaterial.mainTexture = txt;
+        txture.transform.localScale = new Vector3(txt.width, 1, txt.height);
 
     }
 }
