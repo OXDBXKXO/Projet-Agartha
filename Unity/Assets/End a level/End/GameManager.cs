4128ed33a06223c2a226475c35f36706d8ddc70d
Zelta7 <jules.range@epita.fr>
2018-03-12 16:28:18 +0100

    End level added

---
+++ Unity/Assets/End a level/End/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    public GameObject youWinText;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+    }
+
+    public void Win()
+    {   
+        //display a win message
+        //slow down time for dramatic effect
+        //reset the game
+    }
+}
